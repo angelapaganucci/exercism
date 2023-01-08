b8381f5bb9fafc92316a5e52096a129336d0c689
Angela Paganucci <angela.luiz@db1.com.br>
2023-01-08 18:23:54 -0300

    Exercicio Inverte o sinal

---
+++ src/Kata.java
@@ -0,0 +1,12 @@
+public class Kata {
+    public static void main(String[] args) {
+        double result = opposite(-25);
+
+        System.out.println(result);
+
+    }
+
+    public static int opposite(int number) {
+        return number * (-1);
+    }
+}
